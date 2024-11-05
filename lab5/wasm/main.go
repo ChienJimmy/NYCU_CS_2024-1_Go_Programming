3b3ec4776702df733f74f61e92c964a6771006ec
ChienJimmy <jimmychien2006@gmail.com>
2024-11-05 16:13:21 +0800

    2024/11/05 lab5

---
+++ lab5/wasm/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+	"syscall/js"
+)
+
+func CheckPrime(this js.Value, args []js.Value) interface{} {
+	// TODO: Check if the number is prime
+	// Retrieve input value from the HTML input element
+	input := js.Global().Get("document").Call("getElementById", "value").Get("value").String()
+
+	// Convert the input string to a big.Int
+	num := new(big.Int)
+	_, ok := num.SetString(input, 10)
+
+	if !ok {
+		js.Global().Get("document").Call("getElementById", "answer").Set("innerHTML", "Invalid input")
+		return nil
+	}
+
+	// Check if the number is prime
+	if num.ProbablyPrime(0) {
+		js.Global().Get("document").Call("getElementById", "answer").Set("innerHTML", "It's prime")
+	} else {
+		js.Global().Get("document").Call("getElementById", "answer").Set("innerHTML", "It's not prime")
+	}
+
+	return nil
+}
+
+func registerCallbacks() {
+	// TODO: Register the function CheckPrime
+	js.Global().Set("CheckPrime", js.FuncOf(CheckPrime))
+}
+
+func main() {
+	fmt.Println("Golang main function executed")
+	registerCallbacks()
+
+	//need block the main thread forever
+	select {}
+}
